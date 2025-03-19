cf8ba672b8b6982d8d625316f3fcaef3b0ab2e8c
Noa Ligpitan <noaligpitan26@gmail.com>
2025-03-19 10:59:58 +0800

    update .env file and use secret token, not with next_public

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,5 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-18'
 
-export const dataset = "production"
-export const projectId = 'n97onpxb';
+export const dataset = process.env.SANITY_DATASET;
+export const projectId = process.env.SANITY_PROJECT_ID;
