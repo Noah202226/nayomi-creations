9a8fe599a747b8d2ce2da95575f3ab4970bfd40e
Noa Ligpitan <noaligpitan26@gmail.com>
2025-03-19 10:27:32 +0800

    static project to test API

--- sanity/env.js
+++ sanity/env.js
@@ -2,4 +2,4 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-18'
 
 export const dataset = "production"
-export const projectId = 'NEXT_PUBLIC_SANITY_PROJECT_ID';
+export const projectId = 'n97onpxb';
